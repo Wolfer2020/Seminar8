edae3b616f583f7186c2dd0d4c0ae093b297db81
Egor <egr.kuz@mail.ru>
2023-07-11 21:17:05 +0300

    first

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте двумерный массив. Напишите программу, которая 
+//упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+
+        // Заданный массив
+        int[,] array = new int[,]
+        {
+            { 5, 2, 7, 1, 9 },
+            { 3, 8, 4, 6, 2 },
+            { 9, 6, 1, 3, 5 }
+        };
+
+        // Упорядочивание по убыванию для каждой строки
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                for (int k = j + 1; k < array.GetLength(1); k++)
+                {
+                    if (array[i, j] < array[i, k])
+                    {
+                        int temp = array[i, j];
+                        array[i, j] = array[i, k];
+                        array[i, k] = temp;
+                    }
+                }
+            }
+        }
+
+        // Вывод упорядоченного массива
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    
+    
---
+++ Zadacha3/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить 
+//произведение двух матриц.
+
+ // Заданные матрицы
+        int[,] matrix1 = new int[,]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+        };
+
+        int[,] matrix2 = new int[,]
+        {
+            { 7, 8 },
+            { 9, 10 },
+            { 11, 12 }
+        };
+
+        // Размеры матриц
+        int n = matrix1.GetLength(0);
+        int m = matrix1.GetLength(1);
+        int p = matrix2.GetLength(1);
+
+        // Проверка возможности умножения матриц
+        if (m != matrix2.GetLength(0))
+        {
+            Console.WriteLine("Умножение матриц невозможно");
+            return;
+        }
+
+        // Вычисление произведения матриц
+        int[,] result = new int[n, p];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                for (int k = 0; k < m; k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        // Вывод произведения матриц
+        Console.WriteLine("Произведение матриц:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    
